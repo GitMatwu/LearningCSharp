c50e6331927446f0cb00241c443ed62137e45709
Matias Garcia <129994128+GitMatwu@users.noreply.github.com>
2025-04-27 23:24:08 -0300

    Add files via upload

---
+++ ejercicio2/Program.cs
@@ -0,0 +1,19 @@
+﻿Double baC = 0;
+Double Altura = 0;
+Double area = 0;
+Double perimetro = 0;
+
+Console.WriteLine("ingresar la base del rectangulo");
+baC = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("ingresar la altura del rectangulo");
+Altura = Convert.ToDouble(Console.ReadLine());
+
+area = baC * Altura;
+perimetro = (baC * 2) + (Altura * 2);
+
+Console.WriteLine("el area del rectangulo es: " + area);
+Console.WriteLine("el perimetro del rectangulo es: " + perimetro);
+
+
+
