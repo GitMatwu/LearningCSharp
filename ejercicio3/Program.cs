d8f42db80e255fd51613b8922982c68738f235c3
Matias Garcia <129994128+GitMatwu@users.noreply.github.com>
2025-04-27 23:38:45 -0300

    Add files via upload

---
+++ ejercicio3/Program.cs
@@ -0,0 +1,16 @@
+﻿Double Subtotal = 0, Total = 0;
+Double Porcentaje = 0, Descuento = 0;
+
+Console.WriteLine(&quot; Ingresar Importe de la venta &quot;);
+Subtotal = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(&quot; Ingresar porcentaje de descuento &quot;);
+Porcentaje = Convert.ToDouble(Console.ReadLine());
+
+Descuento = Subtotal * Porcentaje / 100;
+
+Total = Subtotal - Descuento;
+
+Console.WriteLine(&quot; Subtotal & quot; +Subtotal.ToString());
+Console.WriteLine(&quot; Descuento & quot; +Descuento.ToString());
+Console.WriteLine(&quot; Total a pagar &quot; +Total.ToString());
